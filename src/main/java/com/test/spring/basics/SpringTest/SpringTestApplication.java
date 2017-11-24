72510c687ea39c1b9270b2f5d20759af169362ab
sachinsharma <sachinsharma@192.168.1.9>
2017-11-23 22:16:48 -0600

    Introduction - Step 4

--- src/main/java/com/test/spring/basics/SpringTest/SpringTestApplication.java
+++ src/main/java/com/test/spring/basics/SpringTest/SpringTestApplication.java
@@ -2,18 +2,21 @@ package com.test.spring.basics.SpringTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(SpringTestApplication.class, args);
+		ApplicationContext appcon = SpringApplication.run(SpringTestApplication.class, args);
+		BinarySearch bs = appcon.getBean(BinarySearch.class);
 		
-		BinarySearch bs = new BinarySearch(new QuickSortAlgo());
+		// BinarySearch bs = new BinarySearch(new QuickSortAlgo());
+		
 		int result = bs.itemSort(new int[] {1,9,5,3},7);
-		
 		System.out.println(result);
+		
 		
 	}
 }
